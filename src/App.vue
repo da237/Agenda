ce8fcb63622d01959de744704e65519255448ad0
Daniel Osorio <daniel951931@outlook.es>
2024-08-17 03:42:25 -0500

    Feat:Se realiza la creacion de los datos en la carpeta de services y se procede a mostarlos en la tabla

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
+import SearchContacs from './components/SearchContacs.vue';
+import ListContacs from './components/ListContacs.vue';
+//
+import { getAllContacts } from './Services/apiContacs'
+const title = "Mi Agenda -Lista de Contactos";
+const listContacts = getAllContacts()
 </script>
 
 <template>
-  <div>
-    
+  <div class="flex items-center flex-col p-5">
+    <h1 class="text-xl m-4">{{ title }}</h1>
+    <SearchContacs/>
+    <ListContacs title="Lista de Resultados"
+    :listContacts="listContacts"/>
+   
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/ListContacs.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-6">
+    <h3 class="text-xl my-3">{{ title }}</h3>
+    <table class="table-fixed">
+      <thead>
+        <th>#</th>
+        <th>Nombre</th>
+        <th>Numero</th>
+        <th>Correo</th>
+        <th>Accion</th>
+      </thead>
+      <tbody>
+        <tr v-for="contact in listContact">
+          <td class="p-3 border-b-2">{{contact.id}}</td>
+          <td class="p-3 border-b-2">{{contact.name}}</td>
+          <td class="p-3 border-b-2">{{contact.phone}}</td>
+          <td class="p-3 border-b-2">{{contact.email}}</td>
+          <td>
+            <a href="" class="py-2 px-4 bg-purple-800 rounded-full">Editar</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  title: {
+    type:String,
+    default:'Lista'
+  },
+  listContact:{
+    type:Array,
+    default: ''
+  }
+});
+</script>
+<style></style>
